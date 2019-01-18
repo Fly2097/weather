9dfbc7d6e459f8a811ab6395e192ca9233f12e7b
Fly <Fly@gmail.com>
2019-01-18 12:03:54 +0800

    First commit.

---
+++ app/src/main/java/com/nastion/fly/weather/util/Utility.java
@@ -0,0 +1,85 @@
+package com.nastion.fly.weather.util;
+
+import android.text.TextUtils;
+import com.nastion.fly.weather.db.City;
+import com.nastion.fly.weather.db.County;
+import com.nastion.fly.weather.db.Province;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by CHW on 2018/6/9.
+ */
+
+public class Utility {
+    /**
+     * jiexi he chuli fuwuqi fanhui de shengjishuju
+     */
+    public static boolean handProvinceResponse(String response){
+        if(!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allProvinces = new JSONArray(response);
+                for(int i = 0;i < allProvinces.length();i++){
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    /**
+     * jiexi he chuli fuwuqi fanhui de shijishuju
+     */
+    public static boolean handCityResponse(String response, int provinceId){
+        if(!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allCities = new JSONArray(response);
+                for(int i = 0;i < allCities.length();i++){
+                    JSONObject cityObject = allCities.getJSONObject(i);
+                    City city = new City();
+                    city.setCityName(cityObject.getString("name"));
+                    city.setCityCode(cityObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    /**
+     * jiexi he chuli fuwuqi fanhui de xianjishuju
+     */
+    public static boolean handCountyResponse(String response, int cityId){
+        if(!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allCounties = new JSONArray(response);
+                for(int i = 0;i < allCounties.length();i++){
+                    JSONObject countyObject = allCounties.getJSONObject(i);
+                    County county = new County();
+                    county.setCountyName(countyObject.getString("name"));
+                    county.setWeatherId(countyObject.getString("weather_id"));
+                    county.setCityId(cityId);
+                    county.save();
+                }
+                return true;
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+}
