9f27d6ae3cf6620cec24f538dbaa92381f7b887d
Fly <Fly@gmail.com>
2019-01-21 16:22:38 +0800

    新增切换城市和手动更新天气的功能。

--- app/src/main/java/com/nastion/fly/weather/ChooseAreaFragment.java
+++ app/src/main/java/com/nastion/fly/weather/ChooseAreaFragment.java
@@ -18,6 +18,7 @@ import android.widget.Toast;
 import com.nastion.fly.weather.db.City;
 import com.nastion.fly.weather.db.County;
 import com.nastion.fly.weather.db.Province;
+import com.nastion.fly.weather.gson.Weather;
 import com.nastion.fly.weather.util.HttpUtil;
 import com.nastion.fly.weather.util.Utility;
 
@@ -92,10 +93,17 @@ public class ChooseAreaFragment extends Fragment {
                     queryCounties();
                 }else if (currentLevel==LEVEL_COUNTY){
                     String weatherId = countyList.get(position).getWeatherId();
-                    Intent intent = new Intent(getActivity(),WeatherActivity.class);
-                    intent.putExtra("weather_id",weatherId);
-                    startActivity(intent);
-                    getActivity().finish();
+                    if (getActivity() instanceof  MainActivity) {
+                        Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                        intent.putExtra("weather_id", weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    }else if(getActivity() instanceof WeatherActivity){
+                        WeatherActivity  activity = (WeatherActivity)getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefreshLayout.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+                    }
                 }
             }
         });
